304d31884ec6d1b4fe12d5254ed65bc339bb4aae
Valeria <valeriascherb@gmail.com>
2020-09-26 12:57:44 +0300

    Complete the zeroth task

--- main.py
+++ main.py
@@ -12,6 +12,15 @@ async def main():
     url = 'wss://sprs.herokuapp.com/{}/{}'.format(task, session)
     async with websockets.connect(url) as ws:
         await ws.send(initial_message)
+        challenge = json.loads(await ws.recv())
+        op1 = challenge['data']['operands'][0]
+        op2 = challenge['data']['operands'][1]
+        op = challenge['data']['operator']
+        if op == '+': res = op1 + op2
+        elif op == '-': res = op1 - op2
+        elif op == '*': res = op1 * op2
+        else: res = 0
+        await ws.send(json.dumps({'data': {'answer': res}}))
         print(await ws.recv())
     pass
 
