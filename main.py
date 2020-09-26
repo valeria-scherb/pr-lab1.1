ab98979499b11a7d2977334a3bb39224fdd35aa8
Valeria <valeriascherb@gmail.com>
2020-09-26 12:11:23 +0300

    Decompose task and session from url

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ import json
 import websockets
 
 initial_message = json.dumps({'data': {'message': "Let's start"}})
+task = 'zeroth'
+session = 'valeria'
 
 async def main():
-    async with websockets.connect('wss://sprs.herokuapp.com/zeroth/valeria') as ws:
+    url = 'wss://sprs.herokuapp.com/{}/{}'.format(task, session)
+    async with websockets.connect(url) as ws:
         await ws.send(initial_message)
         print(await ws.recv())
     pass
