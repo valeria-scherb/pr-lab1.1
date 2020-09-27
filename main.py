10e5038c2e36d07f057ef4821bc97b876f9fcee2
Valeria <valeriascherb@gmail.com>
2020-09-27 15:36:51 +0300

    Try some talking to first task

--- main.py
+++ main.py
@@ -5,23 +5,15 @@ import json
 import websockets
 
 initial_message = json.dumps({'data': {'message': "Let's start"}})
-task = 'zeroth'
+task = 'first'
 session = 'valeria'
 
 async def main():
     url = 'wss://sprs.herokuapp.com/{}/{}'.format(task, session)
     async with websockets.connect(url) as ws:
         await ws.send(initial_message)
-        challenge = json.loads(await ws.recv())
-        op1 = challenge['data']['operands'][0]
-        op2 = challenge['data']['operands'][1]
-        op = challenge['data']['operator']
-        if op == '+': res = op1 + op2
-        elif op == '-': res = op1 - op2
-        elif op == '*': res = op1 * op2
-        else: res = 0
-        await ws.send(json.dumps({'data': {'answer': res}}))
-        print(await ws.recv())
+        info = json.loads(await ws.recv())
+        print(info)
     pass
 
 asyncio.get_event_loop().run_until_complete(main())
