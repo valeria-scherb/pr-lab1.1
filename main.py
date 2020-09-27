0428ce61b9e8a01af287590460580f18530c3aff
Valeria <valeriascherb@gmail.com>
2020-09-27 17:09:37 +0300

    Some more chatting with first task

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ import websockets
 initial_message = json.dumps({'data': {'message': "Let's start"}})
 task = 'first'
 session = 'valeria'
+scale = 5
+settings = {
+    'width': scale, 
+    'height': scale,
+    'noise': 0.1, 
+    'totalSteps': 1,
+    'shuffle': False
+}
 
 async def main():
     url = 'wss://sprs.herokuapp.com/{}/{}'.format(task, session)
@@ -14,6 +22,9 @@ async def main():
         await ws.send(initial_message)
         info = json.loads(await ws.recv())
         print(info)
+        await ws.send(json.dumps({'data': settings}))
+        templ = json.loads(await ws.recv())
+        print(templ)
     pass
 
 asyncio.get_event_loop().run_until_complete(main())
