db1f5349d952a220162f5d5357dec399ffea171f
Valeria <valeriascherb@gmail.com>
2020-10-03 14:18:04 +0300

    Attempt to solve the task (Failed)

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ ready_message = json.dumps({'data': {'message': "Ready"}})
 task = 'first'
 session = 'valeria'
 scale = 5
+p = 0.1
+q = 1.0 - p
 settings = {
     'width': scale, 
     'height': scale,
-    'noise': 0.1, 
+    'noise': p,
     'totalSteps': 1,
     'shuffle': False
 }
@@ -24,16 +26,29 @@ async def main():
         await ws.send(initial_message)
         info = json.loads(await ws.recv())
         print(info)
+        n = scale * info['data']['height']
+        m = scale * info['data']['width']
         await ws.send(json.dumps({'data': settings}))
         templ = json.loads(await ws.recv())
         digits = templ['data']
         await ws.send(ready_message)
         problem = json.loads(await ws.recv())
-        for r in problem['data']['matrix']:
+        x = problem['data']['matrix']
+        for r in x:
             line = ''
             for n in r:
                 line += '# ' if n == 1 else '. '
             print(line)
-    pass
+        probs = {}
+        for k in digits.keys():
+            gk = digits[k]
+            mult = 1
+            for i in range(0, n):
+                for j in range(0, m):
+                    mult *= p ** (x[i][j] ^ gk[i][j])
+                    mult *= q ** (1 ^ x[i][j] ^ gk[i][j])
+            probs[k] = mult
+        sprobs = sorted(probs.items(), key=lambda x: x[1], reverse=True)
+        print(sprobs)
 
 asyncio.get_event_loop().run_until_complete(main())
