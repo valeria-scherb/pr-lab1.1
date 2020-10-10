168359ed18cdaae6c918d7eb4405982887c83129
Valeria <valeriascherb@gmail.com>
2020-10-10 11:38:32 +0300

    Apply ln to algorithm (use sum)

--- recognizer.py
+++ recognizer.py
@@ -4,14 +4,15 @@ Image recognition class
 """
 
 import random as r
+import math
 
 
 class Recognizer:
     def __init__(self):
         self.gks = {}
         self.n = self.m = 0
-        self.p = 0.0
-        self.q = 1.0
+        self.p = self.q = None
+        self.ln_p = self.ln_q = None
 
     def set_size(self, n, m):
         self.n = n
@@ -20,23 +21,25 @@ class Recognizer:
     def set_noise_prob(self, p):
         self.p = p
         self.q = 1.0 - p
+        self.ln_p = math.log(p)
+        self.ln_q = math.log(1.0 - p)
 
     def remember(self, k, g_x):
         self.gks[k] = g_x
 
     def recognize(self, x):
         key = 0
-        value = 0
+        value = -math.inf
         for k in self.gks.keys():
             gk = self.gks[k]
-            mul = 1
+            add = 0
             for i in range(0, self.n):
                 for j in range(0, self.m):
-                    mul *= self.p ** (x[i][j] ^ gk[i][j])
-                    mul *= self.q ** (1 ^ x[i][j] ^ gk[i][j])
-            if mul > value:
+                    add += self.ln_p * (x[i][j] ^ gk[i][j])
+                    add += self.ln_q * (1 ^ x[i][j] ^ gk[i][j])
+            if add > value:
                 key = k
-                value = mul
+                value = add
         return key
 
     def testing_generate_image(self, k, seed=None):
